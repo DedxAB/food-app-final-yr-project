93a6bf13388797b8d835bb593efe1d9038d07e95
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-01-16 23:07:54 +0530

    Update layout and add menu and shopping cart icons

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { Menu, ShoppingCart } from "lucide-react";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-4xl font-bold text-center">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-      <Button className="my-4">Button</Button>
+    <main className="relative h-[87vh] p-5">
+      <div className="flex justify-between items-center">
+        <Menu className="cursor-pointer"/>
+        <Button variant="" size="icon" className="w-10">
+          <ShoppingCart className="h-4 w-4" />
+        </Button>
+      </div>
+
+      <div className="absolute bottom-0 left-0 mt-4 text-center p-5">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+      </div>
     </main>
-  )
+  );
 }
