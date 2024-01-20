4c9a74ecd9ef285db8710b49550f4cbe60117e5b
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-01-20 20:05:06 +0530

    Add link to shopping cart in navigation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { Menu, ShoppingCart } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="relative h-[87vh] p-5">
       <div className="flex justify-between items-center">
-        <Menu className="cursor-pointer"/>
-        <Button variant="" size="icon" className="w-10">
-          <ShoppingCart className="h-4 w-4" />
-        </Button>
+        <Menu className="cursor-pointer" />
+        <Link href="/cart">
+          <Button variant="" size="icon" className="w-10">
+            <ShoppingCart className="h-4 w-4" />
+          </Button>
+        </Link>
       </div>
 
       <div className="absolute bottom-0 left-0 mt-4 text-center p-5">
